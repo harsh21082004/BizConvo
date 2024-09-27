c5481ade7ebe0c1219d251c03149eb603f81239f
Harsh Tiwari <131223935+harsh21082004@users.noreply.github.com>
2024-09-27 18:15:10 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -57,6 +57,13 @@ app.get('/', (req, res) => {
   console.log(Backend is running);
 });
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 
 // Start server
 const PORT = process.env.PORT || 5000;
