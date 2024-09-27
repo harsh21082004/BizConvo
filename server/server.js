eafe1bdefaf07cce6513018bbc1a07cf450507ee
Harsh Tiwari <131223935+harsh21082004@users.noreply.github.com>
2024-09-27 18:11:40 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -61,3 +61,5 @@ app.get('/', (req, res) => {
 // Start server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
