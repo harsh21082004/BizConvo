f1bb9fcccba2c6ef355d9bf506eeab39c25ed25d
Harsh Tiwari <131223935+harsh21082004@users.noreply.github.com>
2024-09-27 18:03:21 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,7 +27,18 @@ app.use(cors({
   credentials: true 
 }));
 
-app.options('*', cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://biz-convo.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  
+  // Respond OK to preflight requests
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
+  next();
+});
 app.use(express.json());
 
 app.use('/uploads', express.static(path.join(__dirname, '../client/public/uploads')));
